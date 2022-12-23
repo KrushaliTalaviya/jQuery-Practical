e45feeb0050a8e30c8f613ac6b5856eb941634fa
Krushali Talaviya <86139380+KrushaliTalaviya@users.noreply.github.com>
2022-12-23 17:43:13 +0530

    Update crud.js

--- crud.js
+++ crud.js
@@ -1,12 +1,12 @@
 $(function() {
-    
-    //for a click on add button
+
+	//for a click on add button
 	$('#btnadd').on('click', function() {
-		
+
 		var id = $("#txtid").val();
 		var country = $("#txtcountry").val();
 		var name = $("#txtname").val();
-        var edit = "<a class='edit' href='JavaScript:void(0);'>Edit</a>";
+		var edit = "<a class='edit' href='JavaScript:void(0);'>Edit</a>";
 		var delet = "<a class='delete' href='JavaScript:void(0);'>Delete</a>";
 		if (name == "" || country == "") {
 			alert("Name and country Filed are Required!");
@@ -15,29 +15,29 @@ $(function() {
 			table = "<tr><td>" + id + "</td><td>" + name + "</td><td>" + country + "</td><td>" + edit + "</td><td>" + delet + "</td></tr>"
 			$("#tbldata").append(table);
 		}
-	    clear();
-        
+		clear();
+
 	});
-    
-    //for a click on clear button
+
+	//for a click on clear button
 	$("#btnclear").on("click", function() {
 		clear();
 	});
-    
-    //for a delete a perticular data row into a table
+
+	//for a delete a perticular data row into a table
 	$("#tbldata").on("click", ".delete", function() {
-        
+
 		if (confirm("Are you sure to want to delete this record")) {
 			$(this).closest('tr').remove();
 		} else {
 			preventDefault();
 		}
-        
+
 	});
-    
-    //for a edit a perticular data row into a table
+
+	//for a edit a perticular data row into a table
 	$("#tbldata").on("click", ".edit", function() {
-        
+
 		var row = $(this).closest('tr');
 		$("#rowindex").val($(row).index());
 		var td = $(row).find("td");
@@ -46,30 +46,30 @@ $(function() {
 		$('#txtcountry').val($(td).eq(2).html());
 		$('#btnadd').hide();
 		$('#btnupdate').show();
-        
+
 	});
-    
-    //for a update a exits data
+
+	//for a update a exits data
 	$('#btnupdate').on('click', function() {
-        
+
 		var id = $("#txtid").val();
 		var name = $("#txtname").val();
 		var country = $("#txtcountry").val();
-        $('#tbldata tbody tr').eq($('#rowindex').val()).find('td').eq(1).html(name);
+		$('#tbldata tbody tr').eq($('#rowindex').val()).find('td').eq(1).html(name);
 		$('#tbldata tbody tr').eq($('#rowindex').val()).find('td').eq(2).html(country);
-        $('#btnadd').show();
+		$('#btnadd').show();
 		$('#btnupdate').hide();
 		clear();
-        
+
 	});
-    
-    //clear the all value in input field
+
+	//clear the all value in input field
 	function clear() {
-        
+
 		$("#txtid").val("");
 		$("#txtname").val("");
 		$("#txtcountry").val("");
 		$("#rowindex").val("");
 	}
-    
+
 });
